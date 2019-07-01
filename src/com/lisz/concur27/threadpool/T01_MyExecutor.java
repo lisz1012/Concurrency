6c3f00b96f94e53ed9fcbaeb21ab64ddca877afc
lisz1012 <lisz1012@163.com>
2019-06-30 17:28:23 -0700

    Executor

---
+++ src/com/lisz/concur27/threadpool/T01_MyExecutor.java
@@ -0,0 +1,22 @@
+/**
+ * 线程池初步：Executor接口，java线程池中最顶层的借口，里面就一个execute方法，
+ * 自己去写实现怎么执行。
+ */
+
+package com.lisz.concur27.threadpool;
+
+import java.util.concurrent.Executor;
+
+public class T01_MyExecutor implements Executor {
+
+	public static void main(String[] args) {
+		new T01_MyExecutor().execute(()->System.out.println("Hello executor!"));
+	}
+
+	@Override
+	public void execute(Runnable command) {
+		new Thread(command).start(); //新起一个线程
+		//command.run();			//直接run，相当于方法调用
+	}
+
+}
